0bd62d6100818f0654bb3daf3a8d9acd2490b187
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-24 11:05:04 +0530

    Update Playlist.dart

--- lib/models/Playlist.dart
+++ lib/models/Playlist.dart
@@ -1,16 +1,37 @@
+import 'Song.dart';
+
 class Playlist {
 
   int id;
-
   String name;
+  // final int userId;
+  final List<Song> songs;
 
   Playlist({
     required this.id,
     required this.name,
+    // required this.userId,
+    required this.songs,
   });
 
-  Playlist.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        name =json['name'];
+  factory Playlist.fromJson(Map<String, dynamic> json) {
+    return Playlist(
+      id: json['id'],
+      name: json['name'],
+      // userId: json['user_id'],
+      songs: (json['songs'] as List<dynamic>?)
+          ?.map((songJson) => Song.fromJson(songJson))
+          .toList() ??
+          [],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'name': name,
+    // 'user_id': userId,
+    'songs': songs.map((song) => song.toJson()).toList(),
+  };
+
 
 }
