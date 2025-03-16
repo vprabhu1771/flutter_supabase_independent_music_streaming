ad451d284315dae43ddd0ef74144d72b43ffe5f0
vprabhu1771 <vprabhu1771@gmail.com>
2025-03-16 10:18:56 +0530

    Update Song.dart

--- lib/models/Song.dart
+++ lib/models/Song.dart
@@ -1,28 +1,37 @@
-class Song {
+import 'Artist.dart';
 
-  int id;
-  String name;
-  String image_path;
-  String song_path;
+class Song {
+  final int id;
+  final String name;
+  final String image_path;
+  final String song_path;
+  final Artist? artist;
 
   Song({
     required this.id,
     required this.name,
     required this.image_path,
-    required this.song_path
+    required this.song_path,
+    this.artist, // Nullable artist
   });
 
-  Song.fromJson(Map<String, dynamic> json)
-      : id = json['id'],
-        name =json['name'],
-        image_path=json['image_path'],
-        song_path=json['song_path'];
+  factory Song.fromJson(Map<String, dynamic> json) {
+    return Song(
+      id: json['id'],
+      name: json['name'],
+      image_path: json['image_path'],
+      song_path: json['song_path'],
+      artist: json['artist'] != null ? Artist.fromJson(json['artist']) : null,
+    );
+  }
 
-  Map<String, dynamic> toJson() => {
-    'id': id,
-    'name': name,
-    'image_path': image_path,
-    'song_path': song_path,
-  };
-
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'image_path': image_path,
+      'song_path': song_path,
+      'artist': artist?.toJson(), // Include artist if not null
+    };
+  }
 }
